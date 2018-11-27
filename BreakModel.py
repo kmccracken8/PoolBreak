f0f0b0374cad21392b94a43ebd5f63180333c0ae
Kyle McCracken <kmccracken@olin.edu>
2018-11-27 16:59:28 -0500

    made vectors

--- BreakModel.py
+++ BreakModel.py
@@ -1,4 +1,15 @@
 from modsim import *
 
-state = State(b1x=1,b1y=0,b1vx=0,b1vy=0)
-params = Params(cx=0,cy=0,cvx=1,cvy=0)
+state = State(cx=0,cy=0,cvx=1,cvy=0,b1x=1,b1y=0,b1vx=0,b1vy=0)
+system = System()
+params = Params()
+
+def slope_func(state, t, system):
+    ids = linrange(0,1,1,endpoint=True)
+    pos_vectors = []
+    vel_vectors = []
+    for b in ids:
+        pos_vectors.append(Vector(state[4*b],state[4*b+1]))
+        vel_vectors.append(Vector(state[4*b+2],state[4*b+3]))
+
+slope_func(state,1,system)
